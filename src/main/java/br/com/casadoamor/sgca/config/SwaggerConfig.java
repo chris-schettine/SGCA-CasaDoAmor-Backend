553ec915c2a9704ca235a9cd6d3ef728fc50558f
Cauê Rodrigues <cauaguiar@outlook.com.br>
2025-10-21 10:00:43 -0300

    feat: criar application-cloud e teste da extensão REST CLIENT

--- src/main/java/br/com/casadoamor/sgca/config/SwaggerConfig.java
+++ src/main/java/br/com/casadoamor/sgca/config/SwaggerConfig.java
@@ -1,5 +1,12 @@
 package br.com.casadoamor.sgca.config;
 
+import org.springdoc.core.customizers.OperationCustomizer;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.MethodParameter;
+import org.springframework.data.domain.Pageable;
+
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -7,12 +14,6 @@ import io.swagger.v3.oas.models.media.Schema;
 import io.swagger.v3.oas.models.parameters.Parameter;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springdoc.core.customizers.OperationCustomizer;
-import org.springdoc.core.models.GroupedOpenApi;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.MethodParameter;
-import org.springframework.data.domain.Pageable;
 import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
