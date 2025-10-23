ab028221357c38c55518b14963842bbaac81ae79
Cauê Rodrigues <cauaguiar@outlook.com.br>
2025-10-23 09:54:58 -0300

    feat: pesquisar usuários com searchText

--- src/main/java/br/com/casadoamor/sgca/repository/auth/AuthUsuarioRepository.java
+++ src/main/java/br/com/casadoamor/sgca/repository/auth/AuthUsuarioRepository.java
@@ -3,12 +3,13 @@ package br.com.casadoamor.sgca.repository.auth;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import br.com.casadoamor.sgca.entity.auth.AuthUsuario;
 
 @Repository
-public interface AuthUsuarioRepository extends JpaRepository<AuthUsuario, Long> {
+public interface AuthUsuarioRepository extends JpaRepository<AuthUsuario, Long>, JpaSpecificationExecutor<AuthUsuario> {
   Optional<AuthUsuario> findByCpf(String cpf);
   Optional<AuthUsuario> findByEmail(String email);
 }
