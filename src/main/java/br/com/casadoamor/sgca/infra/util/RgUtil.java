3b02c1f04f61fbbcff37f438381f8cf817a785f4
João Henrique Silva Pinto <j.henrique.uesb@gmail.com>
2025-11-10 19:04:46 -0300

    style: Format code in RgUtil for consistency

--- src/main/java/br/com/casadoamor/sgca/infra/util/RgUtil.java
+++ src/main/java/br/com/casadoamor/sgca/infra/util/RgUtil.java
@@ -2,10 +2,10 @@ package br.com.casadoamor.sgca.infra.util;
 
 public class RgUtil {
 
-  public static String limparRg(String rg) {
-      if (rg == null) {
-          return null;
-      }
-      return rg.replaceAll("[^0-9]", "");
-  }
+    public static String limparRg(String rg) {
+        if (rg == null) {
+            return null;
+        }
+        return rg.replaceAll("[^0-9]", "");
+    }
 }
