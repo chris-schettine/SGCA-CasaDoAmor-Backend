31c5cc566f26b1dd4f9654ece1c387237d67db15
Cauê Rodrigues <cauaguiar@outlook.com.br>
2025-11-13 09:27:21 -0300

    Update ConsentimentoLGPDController.java

--- src/main/java/br/com/casadoamor/sgca/modules/lgpd/controller/ConsentimentoLGPDController.java
+++ src/main/java/br/com/casadoamor/sgca/modules/lgpd/controller/ConsentimentoLGPDController.java
@@ -1,34 +1,40 @@
 package br.com.casadoamor.sgca.modules.lgpd.controller;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.casadoamor.sgca.infra.util.CpfUtil;
 import br.com.casadoamor.sgca.modules.auth.entity.AuthUsuario;
 import br.com.casadoamor.sgca.modules.auth.repository.AuthUsuarioRepository;
+import br.com.casadoamor.sgca.modules.common.dto.MessageResponseDTO;
 import br.com.casadoamor.sgca.modules.funcionario.entity.Profissional;
 import br.com.casadoamor.sgca.modules.funcionario.repository.ProfissionalRepository;
 import br.com.casadoamor.sgca.modules.lgpd.dto.ConsentimentoLGPDRequestDTO;
 import br.com.casadoamor.sgca.modules.lgpd.dto.ConsentimentoLGPDResponseDTO;
 import br.com.casadoamor.sgca.modules.lgpd.entity.ConsentimentoLGPD.TipoEntidadeLGPD;
 import br.com.casadoamor.sgca.modules.lgpd.service.ConsentimentoLGPDService;
-import br.com.casadoamor.sgca.modules.common.dto.MessageResponseDTO;
-import br.com.casadoamor.sgca.infra.util.CpfUtil;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.Map;
 
 /**
  * Controller unificado para gerenciar consentimentos LGPD de todas as entidades
  */
 @RestController
-@RequestMapping("/api")
+@RequestMapping({"/api", ""})
 @RequiredArgsConstructor
 @Slf4j
 @Tag(name = "LGPD - Consentimentos", description = "Gerenciamento de consentimentos LGPD")
